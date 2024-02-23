db12a8dc8b67efab636656e2c2b3668896c0894a
bun913 <imaizumi.taiki@classmethod.jp>
2024-02-23 09:20:20 +0900

    feat: pidを元にして古いプロセスを消すように調整

--- teams-on-secret-.js
+++ teams-on-secret-.js
@@ -13,8 +13,22 @@
 // Documentation:
 // @raycast.description Teamsを開く時に名前入力と背景ぼかしを自動で入力
 // @raycast.author bun913
-const spawn = require("child_process").spawn;
+const { writeFileSync, existsSync, readFileSync } = require("fs");
+const { spawn } = require("child_process");
+
+// 既にpidファイルがあればプロセスをkillする
+if (existsSync("teams-on-secret.pid")) {
+  try {
+    process.kill(parseInt(readFileSync("teams-on-secret.pid", "utf-8")));
+  } catch (e) {
+    console.log("以前のプロセスが見つかりませんでした");
+  }
+}
 
 const url = process.argv.slice(2)[0];
-const child = spawn("node", ["dist/app.js", url], {detached: true});
+const child = spawn("node", ["dist/app.js", url], { detached: true });
 child.unref();
+
+// pidを取得して、teams-on-secret.pidファイルとして出力する
+const pid = child.pid;
+writeFileSync("teams-on-secret.pid", pid.toString());
