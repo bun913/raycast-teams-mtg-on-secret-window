68bb20c2975ebf722081c69ee6ede4273fb7a85a
bun913 <nagasima.romance+@gmail.com>
2024-03-02 07:42:32 +0900

    feat: open with remote -debbuging

--- teams-on-secret-.js
+++ teams-on-secret-.js
@@ -13,22 +13,7 @@
 // Documentation:
 // @raycast.description Teamsを開く時に名前入力と背景ぼかしを自動で入力
 // @raycast.author bun913
-const { writeFileSync, existsSync, readFileSync } = require("fs");
-const { spawn } = require("child_process");
-
-// 既にpidファイルがあればプロセスをkillする
-if (existsSync("teams-on-secret.pid")) {
-  try {
-    process.kill(parseInt(readFileSync("teams-on-secret.pid", "utf-8")));
-  } catch (e) {
-    console.log("以前のプロセスが見つかりませんでした");
-  }
-}
+const { main } = require("./dist/app");
 
 const url = process.argv.slice(2)[0];
-const child = spawn("node", ["dist/app.js", url], { detached: true });
-child.unref();
-
-// pidを取得して、teams-on-secret.pidファイルとして出力する
-const pid = child.pid;
-writeFileSync("teams-on-secret.pid", pid.toString());
+main(url)
